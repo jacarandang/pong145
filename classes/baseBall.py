dfdfe40f89656b60ad7fce111a69afa5edf431e3
Jym Paul A. Carandang <pauljym_carandang@yahoo.com>
2015-02-14 22:19:22 +0800

    added base classes

---
+++ classes/baseBall.py
@@ -0,0 +1,19 @@
+import random, time
+
+class baseBall:
+
+    def __init__(self, x = 400, y = 300):
+        self.x = x
+        self.y = y
+        self.dx = 0
+        self.dy = 0
+        self.dt = time.time()
+
+    def initiate(self):
+        self.dx = random.choice([-5, 5])
+        self.dy = random.int(-3, 3)
+
+    def update(self):
+        delta = time.time() - self.dt
+        self.x += self.dx*delta
+        self.y += self.dy*delta
