928d509c68427ecea86c37d85282932a70ce6268
jhereme <jhereme_yao@yahoo.com>
2015-05-02 17:25:58 +0800

    multiple clients

--- classes/connections/Client.py
+++ classes/connections/Client.py
@@ -1,29 +1,70 @@
 import socket
+import select
+import sys
 import threading
 
-import ListeningThread
-import TalkingThread
 class Client:
+	def client():
+		HOST = "127.0.0.1"
+		PORT = 8888
+		#HOST = input("Input HOST")
+		#PORT = int(input("Input PORT"))
 		
-	HOST = "127.0.0.1"
-	PORT = 8888
-	
-	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	s.connect((HOST, PORT))
-	
-	t1 = TalkingThread.TalkingThread()
-	t2 = ListeningThread.ListeningThread()
-	
-	t1.start()
-	t2.start()
-	
-	
-	#data = s.recv(1024)
-	#data = bytes.decode(data)
+		csocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		csocket.connect((HOST, PORT))
+		
+		sock_send = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-	#print(data)
+		def send_msg(sock):
+			while 1:
+				message = sys.stdin.readline()
+				sock.send(str.encode(message))
+				#sock.sendto(data, target)
 
-	input("")
+		def recv_msg(sock):
+			while 1:
+				data = sock.recv(1024)
+				data = bytes.decode(data)
+				print(data)
+
+		threading.Thread(target=send_msg, args=(csocket,)).start()  
+		threading.Thread(target=recv_msg, args=(csocket,)).start()
+		
+		#while(1):
+		#	sockets = [socket.socket(), csocket]
+		#	#message = input()
+			
+		#	read, write, error = select.select(sockets, [], [])
+		#	for s in read:
+		#		if(s == csocket):
+		#			data = csocket.recv(1024)
+		#			if not data :
+		#				print ("\nDisconnected from chat server")
+		#				sys.exit()
+		#			else :
+		#				#print data
+		#				sys.stdout.write(bytes.decode(data))
+		#				sys.stdout.write('[Me] '); sys.stdout.flush() 
+		#			data = bytes.decode(data)
+		#			print(data)
+		#		else:
+		#			message = sys.stdin.readLine()
+		#			csocket.send(str.encode(message))
+			
+		#t1 = TalkingThread.TalkingThread()
+		#t2 = ListeningThread.ListeningThread()
+		
+		#t1.start()
+		#t2.start()
+		
 	
+		#data = s.recv(1024)
+		#data = bytes.decode(data)
 
+		#print(data)
+
+		#input("")
+	
+	if __name__ == "__main__":
+		sys.exit(client()) 
 	
--- classes/connections/Server.py
+++ classes/connections/Server.py
@@ -1,14 +1,15 @@
 import socket
-import threading
+import select
+import sys
 
-class Server:
-
-	HOST = "127.0.0.1"
-	PORT = 8888
+#class Server:
 	
-	#clients list
-	clients = []
+HOST = "127.0.0.1"
+PORT = 8888
 	
+#clients list
+sockets = []
+def server():
 	ssocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 	print("Starting server...")
 	
@@ -16,13 +17,47 @@ class Server:
 	ssocket.listen(5)
 	print("Server initialized")
 	
-	while(1):
-		print("Waiting for a connection..")
-		client, address = ssocket.accept()
-		print("new connection from " + str(address))
-		#client.send(str.encode("Shems Co!"))
-		break
+	sockets.append(ssocket)
 	
-
-
+	print("Waiting for a connection..")
+	
+	while(1):
+		# 4th is timeout
+		read, write, error = select.select(sockets, [], [], 0)
+		
+		for s in read:
+			# new client
+			if s == ssocket:
+				client, address = ssocket.accept()
+				sockets.append(client)
+				print(str(client) + " connected!")
+				sendToAll(ssocket, client, "[%s:%s] joined\n" % address)
+			# new message
+			else:
+				try:
+					data = s.recv(1024)
+					if data:
+						data = bytes.decode(data)
+						sendToAll(ssocket, s, data)
+					else: 
+						if s in sockets:
+							sockets.remove(s)
+						sendToAll(ssocket, s, "Client (%s, %s) is offline\n" % address) 
+				except:
+					sendToAll(ssocket, s, "Client (%s, %s) is offline\n" % address) 
+					continue
+	ssocket.close()
+		
+def sendToAll(ssocket, sock, message):
+	for s in sockets:
+		if (s!=ssocket):
+			try:
+				s.send(str.encode(message))
+			except:
+				s.close()
+				if (s in sockets):
+					sockets.remove(s)
+					
+if __name__ == "__main__":
+	sys.exit(server()) 
 	
